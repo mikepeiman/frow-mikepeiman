0ead722056c68bfa1401dff6a1b01cb69070a615
Mike Peiman <mikepeiman@gmail.com>
2017-06-24 19:33:45 -0700

    I don't remember what changes these were. Didn't commit last week when I made them

--- js/title-text-animation.js
+++ js/title-text-animation.js
@@ -1,9 +1,5 @@
-var $title-h1 = $('#title-h1');
-var $title-h2 = $('#title-h2')
+    $("#titleh1").blast({ delimiter: "word" }); 
+    $("#titleh2").blast({ delimiter: "character" });
 
-$("#title-h1").blast({ delimiter: "word" }); 
-$("#title-h2").blast({ delimiter: "character" });
-$("div").blast({ delimiter: "character" });
-
-$title-h1.velocity({ width: "300px" }, { duration 500, delay: 1000 });
-$title-h2.velocity({ opacity: 1 }, { duration 500, delay: 1000 });
+    $("#titleh1").velocity({ left: "300px" }, { duration 500, delay: 1000 });
+    $("#titleh2").velocity({ opacity: 0 }, { duration 500, delay: 1000 });
