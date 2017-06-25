f5214f7ff1fd89a30cf271326c6f0f63a329e75c
Mike Peiman <mikepeiman@gmail.com>
2017-06-24 21:36:52 -0700

    got text animations working more or less as I want; next is a slideshow effect, then tweaking each page bg and style, then content, then publish

--- js/title-text-animation.js
+++ js/title-text-animation.js
@@ -3,3 +3,22 @@
 
     $("#titleh1").velocity({ left: "300px" }, { duration 500, delay: 1000 });
     $("#titleh2").velocity({ opacity: 0 }, { duration 500, delay: 1000 });
+
+    ===
+
+    $(document).ready(function() {
+$(".lead-paragraph-home").velocity("fadeIn", { duration: 1500 });
+$("#titleh2").blast({ delimiter: "character", customClass: "subtitleLetter" });
+
+var textAnimation = [{
+   elements: $('.subtitleLetter'),
+  p: 'transition.swoopIn',
+  options: {
+    duration: 1000,
+    stagger: 50,
+    backwards: true,
+  }
+}];
+
+$.Velocity.RunSequence(textAnimation);
+});
